a1a8b9a6e3beb6496ba207bc49b77e816ce677be
zerune1 <abbasakindele@gmail.com>
2026-08-01 01:30:04 +0100

    Create supabaseClient.js

---
+++ js/supabaseClient.js
@@ -0,0 +1,7 @@
+export const SUPABASE_URL = "https://bleejhbxqxdvnrrisdbm.supabase.co";
+export const SUPABASE_ANON_KEY = "sb_publishable_Z2-9Gq6qgCY4GFhOJA7FjQ_DPS21aRg
+";
+
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
