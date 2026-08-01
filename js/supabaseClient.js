95b9911205d119053328c018aa18a77b0820dafe
zerune1 <abbasakindele@gmail.com>
2026-08-01 09:16:08 +0100

    Fix formatting of SUPABASE_ANON_KEY export

--- js/supabaseClient.js
+++ js/supabaseClient.js
@@ -1,6 +1,5 @@
 export const SUPABASE_URL = "https://bleejhbxqxdvnrrisdbm.supabase.co";
-export const SUPABASE_ANON_KEY = "sb_publishable_Z2-9Gq6qgCY4GFhOJA7FjQ_DPS21aRg
-";
+export const SUPABASE_ANON_KEY = "sb_publishable_Z2-9Gq6qgCY4GFhOJA7FjQ_DPS21aRg";
 
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
